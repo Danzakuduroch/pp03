d0c2999c7f4e1ca35bcc39e5fdb1908981ef59ae
Danzakuduroch <rassistavanturist7@gmail.com>
2025-09-22 19:53:36 +0500

    добавлены стили

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AppController::class, "index"]);
+
+Route::get('/about', [AppController::class, "aboutShow"])->name("about");
+Route::get('/promotions', [AppController::class, "promotionsShow"])->name("promotions");
+Route::get('/account', [AppController::class, "accountShow"])->name("account");

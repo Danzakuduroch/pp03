fcf7b79bc3e6fe40eb95bc5b0de915acf53811ed
Danzakuduroch <rassistavanturist7@gmail.com>
2025-09-22 19:16:56 +0500

    добавлены компоненты

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,24 @@
+<header class="header">
+    <div class="header__inner container">
+        <img class="logo" src="/assets/images/logo.png" alt="логотип">
+        <nav class="nav">
+            <ul class="nav__list">
+                <li class="nav__item">
+                    <a href={{ route("about") }}>
+                        О нас
+                    </a>
+                </li>
+                <li class="nav__item">
+                    <a href={{ route("promotions") }}>
+                        Акции
+                    </a>
+                </li>
+                <li class="nav__item">
+                    <a href={{ route("account") }}>
+                        Аккаунт
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
